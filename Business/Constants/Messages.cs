c91e850dbf6e46f677a27d1dae9d25bbe5906979
Bedirhan Karaahmetli <bedoo1905@outlook.com>
2021-02-11 00:04:25 +0300

    10.02.21

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Entities.Concrete;
 
 namespace Business.Constants
 {
@@ -8,5 +9,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Product Succesfully Added";
         public static string ProductNameInvalid = "The product name must be at least 2 characters!";
+        internal static string MaintenanceTime = "System is under maintenance";
+        internal static string ProductsListed = "Products are listed";
     }
 }
